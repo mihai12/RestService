7e93e5861d74b1ac59ca9d38fab607fa01e277f6
mihai12 <mihai.popa@windowslive.com>
2016-11-02 16:08:24 +0200

    Initial commit and initial RestService

---
+++ RestService/IRestServiceImpl.cs
@@ -0,0 +1,29 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestServiceImpl" in both code and config file together.
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+
+        //XML
+        [OperationContract]
+        [WebInvoke(
+            Method          = "GET",
+            ResponseFormat  = WebMessageFormat.Xml,
+            BodyStyle       = WebMessageBodyStyle.Wrapped,
+            UriTemplate     = "xml/{id}")]
+        string XMLData(string id);
+
+        //JSON
+        [OperationContract]
+        [WebInvoke(
+            Method          = "GET",
+            ResponseFormat  = WebMessageFormat.Json,
+            BodyStyle       = WebMessageBodyStyle.Wrapped,
+            UriTemplate     = "json/{id}")]
+        string JSONData(string id);
+    }
+}
---
+++ RestService/RestServiceImpl.svc.cs
@@ -0,0 +1,21 @@
+﻿namespace RestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestServiceImpl" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestServiceImpl.svc or RestServiceImpl.svc.cs at the Solution Explorer and start debugging.
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        #region IRestService Members
+
+        public string XMLData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        public string JSONData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        #endregion
+    }
+}
